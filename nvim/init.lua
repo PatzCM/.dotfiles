9c20782bb2647519d3ef5f7edb885fadb8891ff0
Patricia Alexandra Cesar Monteiro <palexand@c1r5s9.42porto.com>
2024-10-18 16:58:49 +0100

    my git

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+-- Core
+-- Global options
+require("patz.core.global")
+-- -- Options
+require("patz.core.options")
+-- -- Key Mappings
+require("patz.core.mappings")
+-- -- Auto Commands
+require("patz.core.autocmd")
+-- -- Macros
+require("patz.core.macros")
+--
+-- -- Lazy Plugin Manager
+require("patz.lazy")
