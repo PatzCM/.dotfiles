28adc389fb92ab7b18587fae19238fecbcc18cc6
Patrícia Monteiro <patz@Patricias-MacBook-Air.local>
2024-10-30 16:21:21 +0000

    Added nvim & .zshrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,14 @@
 -- Core
 -- Global options
-require("patz.core.global")
--- -- Options
-require("patz.core.options")
--- -- Key Mappings
-require("patz.core.mappings")
--- -- Auto Commands
-require("patz.core.autocmd")
--- -- Macros
-require("patz.core.macros")
---
--- -- Lazy Plugin Manager
-require("patz.lazy")
--- -- Mini Icons
-require('mini.icons').setup()
+require("zedro.core.global")
+-- Options
+require("zedro.core.options")
+-- Key Mappings
+require("zedro.core.mappings")
+-- Auto Commands
+require("zedro.core.autocmd")
+-- Macros
+require("zedro.core.macros")
+
+-- Lazy Plugin Manager
+require("zedro.lazy")
--- nvim/lua/patz/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	'tmux-plugins/vim-tmux',
-	'echasnovski/mini.icons', version = '*', --mini icons
-	"nvim-lua/plenary.nvim", --plenary
-}
